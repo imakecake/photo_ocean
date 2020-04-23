2c8fc8f951051f879beab6098f7f9e2e20deac54
imakecake <coras49@mail.ru>
2020-04-24 00:16:14 +0300

    Little changes in Home page and app helper for show full title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #Возвращает полный заголовок на основе заголовка страницы.
+  def full_title(page_titile = '')
+    base_title = "Photo Ocean"
+    if page_titile.empty?
+      base_title
+    else
+      page_titile + "|" base_title
+    end
+  end
+
 end
