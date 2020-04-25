0a0320a8035aa63ca1b31d42dcc19f1cb55eea14
imakecake <coras49@mail.ru>
2020-04-26 00:15:58 +0300

    Complete basic user model with base testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  before_save { self.email= email.downcase }
+  before_save { email.downcase! } # берет переменную экземпляра, применяет к ней downcase сохраняя изменения в той же переменной
   validates :name, presence: true, length: {maximum:45}
-  #VALID_EMAIL_REGEX = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #VALID_EMAIL_REGEX = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ # Device?
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, # требование не пустого поля
                     length: {maximum:255},  # ограничение длины в 255 символов (максимум для varchar в БД)
                     format: { with: VALID_EMAIL_REGEX }, # соответствие регулярке
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @base_title = "Photo Ocean"
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,9 +13,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    #get signup_path
-    assert_select "a[href=?]", signup_path #users_new_path
+    assert_select "a[href=?]", signup_path
+    get contact_path
+  end
+
+  test "should get sign up" do
     get signup_path
+    assert_response :success
+    assert_select "title", "Sign up | #{@base_title}"
   end
 
 end
